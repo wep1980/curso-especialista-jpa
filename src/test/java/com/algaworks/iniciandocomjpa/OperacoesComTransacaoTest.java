eef23522424a339b4c13727bd9adbd8de528d23c
wep1980 <wepcienciadacomputacao@gmail.com>
2024-12-30 13:43:27 -0300

    Adicionando objetos com o método merge

--- src/test/java/com/algaworks/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -11,6 +11,26 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
 
 
     @Test
+    public void inserirObjetoComMerge() {
+        Produto produto = new Produto();
+
+        produto.setId(4);
+        produto.setNome("Microfone Rode Videmic");
+        produto.setDescricao("A melhor qualidade de som.");
+        produto.setPreco(new BigDecimal(1000));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertNotNull(produtoVerificacao);
+    }
+
+
+    @Test
     public void atualizarObjetoGerenciado() {
         Produto produto = entityManager.find(Produto.class, 1);
 
